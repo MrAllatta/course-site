cfa56b82065aafac4e041df24e60b1aab1245cf3
MrAllatta <eallatta@afsenyc.org>
2018-11-19 00:23:43 -0500

    simplified build

--- my-build.rkt
+++ my-build.rkt
@@ -1,34 +1,13 @@
 #!/usr/bin/env racket
 #lang racket/base
-(require racket/runtime-path
-         racket/system
-         racket/string
-         racket/cmdline
-         racket/path
-         racket/file
-         racket/list
-         racket/match
-         racket/vector
-         (for-syntax racket/base)
-         (planet neil/csv:1:=7)
-         "lib/system-parameters.rkt"
-         "lib/translate-pdfs.rkt"
-         "lib/paths.rkt"
-         "lib/build-helpers.rkt"
-         "lib/build-modes.rkt"
-         "lib/scribble-pdf-helpers.rkt"
-         "lib/pdf-lesson-exercises.rkt"
-         "lib/warnings.rkt"
-         scribble/render
+(require scribble/render
          scribble/html-render
          file/zip
 	 scribble/html-properties
          "intro/langs/en-us/course-main.scrbl")
 
-
-
 (render (list doc)
         (list "index")
         #:render-mixin (compose render-multi-mixin render-mixin)
         #:dest-dir "distribution"
-	#:style-extra-files (list "courses/intro/langs/en-us/my-style.css"))
+	#:style-extra-files (list "intro/langs/en-us/my-style.css"))
